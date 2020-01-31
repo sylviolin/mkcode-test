b17d3bd776676ac5870834404b69004323a2d6ce
Sylvia Lin <SylviaLin@via.com.tw>
2020-01-31 15:52:06 +0800

    Define pixetto APIs.

--- main.ts
+++ main.ts
@@ -28,6 +28,42 @@ namespace pixetto {
 	export function ss_init(rx: DigitalPin, tx: DigitalPin) {
 
     }
+    
+    //% block="識別到物體"
+    export function expression(): boolean {
+        return false;
+    }
+    
+    //% block="目前功能"
+    export function getFuncID(): number {
+        return 0;
+    }
+    
+    //% block="物體類別"
+    export function getTypeID(): number {
+        return 0;
+    }
+    
+    //% block="物體座標X"
+    export function getPosX(): number {
+        return 0;
+    }
+    
+    //% block="物體座標Y"
+    export function getPosY(): number {
+        return 0;
+    }
+
+    //% block="物體寬度"
+    export function getWidth(): number {
+        return 0;
+    }
+
+    //% block="物體高度"
+    export function getHeight(): number {
+        return 0;
+    }
+
     /**
     * 計算長方形面積，並回傳
     */

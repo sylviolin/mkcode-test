7b132440e646c5e8e5f378fd73d2bf8faa29cfd7
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-12 17:33:08 +0800

    Replace enum pins with microbit's pins.

--- main.ts
+++ main.ts
@@ -1,36 +1,19 @@
 enum pins {
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    A0,
-    A1,
-    A2,
-    A3,
-    A4,
-    A5
+    P0,
+    P1,
+    P2
 }
 
 //% weight=0 color=#A88002 icon="\uf0ad" block="pixetto"
 namespace pixetto {
 
 	//% block="初始化視覺感測器 RX# %rx TX# %tx"
-	export function ss_init(rx: DigitalPin, tx: DigitalPin) {
+	export function ss_init(rx: pins, tx: pins) {
 
     }
     
     //% block="識別到物體"
-    export function expression(): boolean {
+    export function isDetected(): boolean {
         return false;
     }
     
@@ -59,25 +42,11 @@ namespace pixetto {
         return 0;
     }
 
-    //% block="物體高度"
-    export function getHeight(): number {
-        return 0;
-    }
-
     /**
     * 計算長方形面積，並回傳
     */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
-    }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
+    //% block="物體高度"
+    export function getHeight(): number {
+        return 0;
     }
 }

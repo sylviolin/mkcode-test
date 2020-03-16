6f9b7b1a9ebe6bf282515569424d919e860293e0
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-16 14:53:39 +0800

    add new files

---
+++ enums.d.ts
@@ -0,0 +1,5 @@
+// Auto-generated. Do not edit.
+declare namespace pixetto {
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ shims.d.ts
@@ -0,0 +1,9 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% color=#D400D4 weight=111 icon="\uf192"
+declare namespace pixetto {
+}
+
+// Auto-generated. Do not edit. Really.

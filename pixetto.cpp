e9fabfd108236229ef34990fd3e24f8b037e7a46
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-17 11:44:03 +0800

    test32

--- pixetto.cpp
+++ pixetto.cpp
@@ -32,23 +32,23 @@ namespace pixetto {
     int isDetected(){
 		uint8_t data_buf[10] = {0};
 		int read_len = 0;
-		uBit.serial.send("detected ");
 		do {
 			read_len = serial->read(data_buf, 1);
 			if (read_len == 0) return 0;
 		} while (data_buf[0] != PXT_PACKET_START);
-		uBit.serial.send("start ");
+		return 8;
+		/*
 		int i = 0;
 		do {
 			i++;
 			read_len = serial->read(&data_buf[i], 1);
 			if (read_len == 0) return 0;
 		} while (data_buf[i] != PXT_PACKET_END && i < 9);
-		uBit.serial.send("end ");
 		//return 5;
 		if (data_buf[2] < 20)
 			return data_buf[2];
 		else
 			return 0;
+		*/
 	}
 }

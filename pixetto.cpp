78373f30b772b3b91967b5f709e1c8a3bed628fa
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-17 11:13:16 +0800

    test 30

--- pixetto.cpp
+++ pixetto.cpp
@@ -5,19 +5,26 @@
 #define PXT_PACKET_END   	0xFE
 #define PXT_CMD_STREAMON	0x79
 
+using namespace pxt;
+
 //% color=#D400D4 weight=111 icon="\uf192"
 namespace pixetto {
 	MicroBitSerial *serial=nullptr;
 	
     //% 
     void begin(PinName rx, PinName tx){
-		uint8_t data_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON, 0, PXT_PACKET_END};
-        serial=new MicroBitSerial(rx, tx);
-        serial->baud(38400);
-        uBit.sleep(100);
-        
-        serial->send(data_buf, 5);
-        //uBit.sleep(1000);
+		PinName txn;
+		PinName rxn;
+		//if (tryResolvePin(tx, txn) && tryResolvePin(rx, rxn))
+		{
+			serial=new MicroBitSerial(MICROBIT_PIN_P2, MICROBIT_PIN_P1);//(txn, rxn);
+			serial->baud(38400);
+			uBit.sleep(100);
+
+			uint8_t data_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON, 0, PXT_PACKET_END};
+			serial->send(data_buf, 5);
+			//uBit.sleep(1000);
+		}
     }
     
     //%

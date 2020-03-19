6e3d4c0df0ec98bd0b29511c3a2c56c91e454f08
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-19 14:22:06 +0800

    test63

--- pixetto.cpp
+++ pixetto.cpp
@@ -65,18 +65,28 @@ namespace pixetto {
 			if (read_len == 0) return 0;
 		} while (data_buf[0] != PXT_PACKET_START);
 
+		/*
 		int i = 1;
 		do {
 			read_len = serial->read(&data_buf[i], 1);
 			if (read_len == 0) continue; //return 0;
 			i++;
 		} while (data_buf[i-1] != PXT_PACKET_END && i < 10);
+		*/
+		
+		read_len = serial->read(&data_buf[1], 9);
+		
+		int aa = 0;
+		if (data_buf[0] == PXT_PACKET_START) aa += 50000;
+		if (data_buf[9] == PXT_PACKET_END) aa += 6000;
+		if (data_buf[1] == 0x0A) aa += 700;
+		aa = aa + (data_buf[2] * 10) + data_buf[3];
+		return aa;
+		
+		/*if (data_buf[8] == PXT_PACKET_END) return 8;
+		
 		if (data_buf[0] == PXT_PACKET_START) return 5;
 		else return 9;
-		/*
-		//return i;
-		//read_len = serial->read(&data_buf[1], 9);
-		//if (data_buf[8] == PXT_PACKET_END) return 8;
 		
 		
 		if (data_buf[2] < 10)

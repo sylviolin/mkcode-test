94f41b714712377d6e66fed0d0308d63c8592a3b
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-20 10:23:34 +0800

    test81

--- pixetto.cpp
+++ pixetto.cpp
@@ -65,7 +65,7 @@ namespace pixetto {
 			read_len = serial->read(data_buf, 1, ASYNC);
 			//if (read_len == 0) continue; //return 0;
 			if (read_len == MICROBIT_SERIAL_IN_USE) return 64;
-			if (read_len == 0) return 0;
+			//if (read_len == 0) return 0;
 		} while (data_buf[0] != PXT_PACKET_START);
 
 		/*

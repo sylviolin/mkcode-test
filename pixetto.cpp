90f583df8e8d176ff29fb097f9753b8545c67e46
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-23 14:23:13 +0800

    test100

--- pixetto.cpp
+++ pixetto.cpp
@@ -51,7 +51,8 @@ namespace pixetto {
 			if (code_buf[1] == 5) aa += 20;
 			if (code_buf[2] == PXT_RET_CAM_SUCCESS) aa +=300;
 			if (code_buf[4] == PXT_PACKET_END) aa += 4000;
-			return aa;
+			//return aa;
+			return pixdata->posx;
 		}
     }
     

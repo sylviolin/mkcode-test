f7bafd365cf8983c8aa90d883028dbb8c2082d1b
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-25 14:55:54 +0800

    test113

--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,25 @@
 // Auto-generated. Do not edit.
-declare namespace pixetto {
-}
+
+
+    declare const enum SerialPin2 {
+    //% block="P0"
+    PP0 = 0,
+    //% block="P1"
+    PP1 = 1,
+    //% block="P2"
+    PP2 = 2,
+    //% block="P8"
+    PP8 = 8,
+    //% block="P12"
+    PP12 = 12,
+    //% block="P13"
+    PP13 = 13,
+    //% block="P14"
+    PP14 = 14,
+    //% block="P15"
+    PP15 = 15,
+    //% block="P16"
+    PP16 = 16,
+    }
 
 // Auto-generated. Do not edit. Really.

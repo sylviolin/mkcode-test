cb726ad3ea387a490ca1c874370aee9a7496df92
Sylvia Lin <SylviaLin@via.com.tw>
2020-03-25 11:33:55 +0800

    test109

--- main.ts
+++ main.ts
@@ -1,50 +1,50 @@
 //% weight=0 color=#A88002 icon="\uf0ad" block="Pixetto"
 namespace PIXETTO {
-
 	//% block="初始化視覺感測器 RX %rx TX %tx"
-	export function begin(rx: SerialPin2, tx: SerialPin2): number {
+	//% shim=pixetto::begin
+	function begin(rx: SerialPin2, tx: SerialPin2): number {
 		return 0
     }
     
     //% block="識別到物體"
     //% shim=pixetto::isDetected
-    export function isDetected(): number {
+    function isDetected(): number {
 		return 0
     }
     
     //% block="目前功能"
     //% shim=pixetto::getFuncID
-    export function getFuncID(): number {
+    function getFuncID(): number {
         return 0;
     }
     
     //% block="物體類別"
     //% shim=pixetto::getTypeID
-    export function getTypeID(): number {
+    function getTypeID(): number {
         return 0;
     }
     
     //% block="物體座標X"
     //% shim=pixetto::getPosX
-    export function getPosX(): number {
+    function getPosX(): number {
         return 0;
     }
     
     //% block="物體座標Y"
     //% shim=pixetto::getPosY
-    export function getPosY(): number {
+    function getPosY(): number {
         return 0;
     }
 
     //% block="物體寬度"
     //% shim=pixetto::getWidth
-    export function getWidth(): number {
+    function getWidth(): number {
         return 0;
     }
 
     //% block="物體高度"
     //% shim=pixetto::getHeight
-    export function getHeight(): number {
+    function getHeight(): number {
         return 0;
     }
 }

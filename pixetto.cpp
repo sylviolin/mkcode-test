44a5884ce2ece5b18e081f52b7c5edbc0882a2ee
Sylvia Lin <SylviaLin@via.com.tw>
2020-04-16 16:40:59 +0800

    test147

--- pixetto.cpp
+++ pixetto.cpp
@@ -147,8 +147,8 @@ namespace pixetto {
 		return true;
 	}
 	//%
-	bool getFuncID(PixFunction func_id){
-		return data_buf[2] == func_id;
+	bool getFuncID(int func_id){
+		return (data_buf[2] == func_id);
 	}
 	//%
 	int getTypeID() {

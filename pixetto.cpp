d678e427273499dd4fe58386dc0116028b9e8997
Sylvia Lin <SylviaLin@via.com.tw>
2020-12-15 15:30:40 +0800

    test30

--- pixetto.cpp
+++ pixetto.cpp
@@ -19,30 +19,8 @@
 #define SERIAL_BUF_SIZE		64
 #define DATA_SIZE			33
 
-//using namespace codal;
+using namespace codal;
 //using namespace pxt;
-
-Pin *edgeConnector[] = {
-    &uBit.io.P0, 
-    &uBit.io.P1, 
-    &uBit.io.P2, 
-    &uBit.io.P3, 
-    &uBit.io.P4, 
-    &uBit.io.P5, 
-    &uBit.io.P6, 
-    &uBit.io.P7, 
-    &uBit.io.P8, 
-    &uBit.io.P9, 
-    &uBit.io.P10, 
-    &uBit.io.P11, 
-    &uBit.io.P12, 
-    &uBit.io.P13, 
-    &uBit.io.P14, 
-    &uBit.io.P15, 
-    &uBit.io.P16, 
-    &uBit.io.P19, 
-    &uBit.io.P20
-};
 
 namespace pixetto {
 	Serial *serial = nullptr;
@@ -58,10 +36,10 @@ namespace pixetto {
 	
     //% 
     bool begin() { 
-		//uBit.init();
+		uBit.init();
 		bool ret = false;
 		
-		//uBit.sleep(3000);
+		uBit.sleep(3000);
 		//Pin aa = uBit.io.P0;
 		//Pin bb = uBit.io.P1;
 		//serial = new Serial(&uBit.io.P0, &uBit.io.P1);
@@ -71,24 +49,6 @@ namespace pixetto {
 		//uBit.serial.printf("data");
 		//uBit.serial.setBaudrate(38400);
 		//uBit.sleep(100);
-
-    uBit.display.disable();
-    uBit.buttonA.disable();
-    uBit.buttonB.disable();
-
-    for (Pin *p : edgeConnector)
-        p->setDigitalValue(0);
-
-    while(1)
-    {
-        for (Pin *p : edgeConnector)
-        {
-            p->setDigitalValue(1);
-            uBit.sleep(500);
-            p->setDigitalValue(0);
-            uBit.sleep(500);
-        }
-    }
 
 		return ret;
     }

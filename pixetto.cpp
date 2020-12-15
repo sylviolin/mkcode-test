edc46fee13abe358015f258266a0767ce3dd7cc1
Sylvia Lin <SylviaLin@via.com.tw>
2020-12-15 17:03:40 +0800

    test40

--- pixetto.cpp
+++ pixetto.cpp
@@ -246,8 +246,8 @@ namespace pixetto {
 				serial = new MicroBitSerial(txn, rxn, 64, 20);
 				
 
-			//serial->baud(38400);
-			serial->setBaudrate(38400);
+			serial->baud(38400);
+			//serial->setBaudrate(38400);
 			//serial->setRxBufferSize(64);
 			//serial->setTxBufferSize(32);
 			uBit.sleep(100);
@@ -320,8 +320,8 @@ namespace pixetto {
 			if (serial == nullptr)
 				serial = new MicroBitSerial(txn, rxn, 64, 20);
 
-			//serial->baud(38400);
-			serial->setBaudrate(38400);
+			serial->baud(38400);
+			//serial->setBaudrate(38400);
 			//serial->setRxBufferSize(64);
 			//serial->setTxBufferSize(32);
 			uBit.sleep(100);

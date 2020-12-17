278606c2d2a2c9bf60da14fecfbbc2985ecfc2b9
Sylvia Lin <SylviaLin@via.com.tw>
2020-12-17 10:49:15 +0800

    test56

--- pixetto.cpp
+++ pixetto.cpp
@@ -566,7 +566,8 @@ namespace pixetto {
 			return 1;
 
 		
-		if (read_len != (data_len-3)) return 2;
+		//if (read_len != (data_len-3)) return 2;
+		if (readidx != data_len) return 2;
 		if (data_buf[data_len-1] != PXT_PACKET_END) return 3;
 		if (!verifyChecksum(data_buf, data_len)) return 4;
 		if (data_buf[2] == 0) return 5; // num == 0

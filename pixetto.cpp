27935b23486df8a5f2260ea2402d93c294f90df8
Sylvia Lin <SylviaLin@via.com.tw>
2020-12-17 16:04:08 +0800

    test24

--- pixetto.cpp
+++ pixetto.cpp
@@ -8,6 +8,8 @@
 #define MICROBIT_CODAL 1
 #else
 #define MICROBIT_CODAL 0
+#define target_panic(n) microbit_panic(n)
+#define target_wait(n) wait_ms(n)
 #endif
 
 #define PXT_PACKET_START 	0xFD
@@ -35,11 +37,93 @@ enum PixSerialPin {
     P16 = 16
 };
 
+enum PixFunction {
+        //% block="Color Detection"
+        COLOR_DETECTION=1,
+        //% block="Color Codes Detection"
+        COLOR_LABEL=2,
+        //% block="Shape Detection"
+        SHAPE_DETECTION=3,
+        //% block="Sphere Detection"
+        SPHERE_DETECTION=4,
+        //% block="Template Matching"
+        TEMPLATE=6,
+        //% block="Keypoint"
+        KEYPOINT=8,
+        //% block="Neural Network"
+        NEURAL_NETWORK=9,
+        //% block="AprilTag(16h5)"
+        APRILTAG=10,
+        //% block="Face Detection"
+        FACE_DETECTION=11,
+        //% block="Traffic Sign Detection"
+        TRAFFIC_SIGN_DETECTION=12,
+        //% block="Handwritten Digits Detection"
+        HANDWRITTEN_DIGITS_DETECTION=13,
+        //% block="Handwritten Letters Detection"
+        HANDWRITTEN_LETTERS_DETECTION=14,
+        //% block="Remote Computing"
+        REMOTE_COMPUTING=15,
+        //% block="Lanes Detection"
+        LANES_DETECTION=16,
+        //% block="Digits Operation"
+        DIGITS_OPERATION=17,
+        //% block="Simple Classifier"
+        SIMPLE_CLASSIFIER=18,
+        //% block="Voice Commands"
+        VOICE_COMMANDS=19
+};
+    
+enum PixApriltagField {
+        //% block="position x"
+        APRILTAG_POS_X=1,
+        //% block="position y"
+        APRILTAG_POS_Y,
+        //% block="position z"
+        APRILTAG_POS_Z,
+        //% block="rotation x"
+        APRILTAG_ROT_X,
+        //% block="rotation y"
+        APRILTAG_ROT_Y,
+        //% block="rotation z"
+        APRILTAG_ROT_Z,
+        //% block="center x"
+        APRILTAG_CENTER_X,
+        //% block="center y"
+        APRILTAG_CENTER_Y
+};
+
+enum PixLanesField {
+        //% block="Left X1"
+        LANES_LX1=1,
+        //% block="Left Y1"
+        LANES_LY1,
+        //% block="Left X2"
+        LANES_LX2,
+        //% block="Left Y2"
+        LANES_LY2,
+        //% block="Right X1"
+        LANES_RX1,
+        //% block="Right Y1"
+        LANES_RY1,
+        //% block="Right X2"
+        LANES_RX2,
+        //% block="Right Y2"
+        LANES_RY2
+};
+
 //using namespace pxt;
 //using namespace codal;
 
+//#if MICROBIT_CODAL
+//#error "This is V2"
+//#else
+//#error "This is V1"
+//#endif
+
 namespace pixetto {
 	//MicroBit uBit;
+	
 	MicroBitSerial *serial = nullptr;
 	uint8_t data_buf[DATA_SIZE] = {0xFF};
 	int data_len = 0;
@@ -53,7 +137,7 @@ namespace pixetto {
 	char m_eqExpr[17] = {0};
 	float m_posx=0, m_posy=0, m_posz=0, m_rotx=0, m_roty=0, m_rotz=0, m_centerx=0, m_centery=0;
 	bool bOnStarting = false;
-
+	
     bool getPinName(PixSerialPin p, PinName& name) {
       switch(p) {
         case P0: name = MICROBIT_PIN_P0;  return true;
@@ -68,8 +152,8 @@ namespace pixetto {
       }
       return false;
     }
-    
-    bool ssflush()
+	
+	bool ssflush()
 	{
 		uint8_t a;
 		
@@ -100,7 +184,58 @@ namespace pixetto {
 
 		return read_idx;
 	}
+	
+	bool verifyChecksum(uint8_t *buf, int len)
+	{
+		uint8_t sum = 0;
+		
+		for (uint8_t i=1; i<len-2; i++)
+			sum += buf[i];
+		
+		if (sum == PXT_PACKET_START || sum == PXT_PACKET_END || sum == 0xFF)
+			sum = 0xAA;
+		
+		return (sum == buf[len-2]);
+	}
+	
+	bool checkcam()
+	{
+		ssflush();
+		
+		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON_CB, 0, PXT_PACKET_END};
+		serial->send(cmd_buf, 5, ASYNC);
+			
+		int read_len = 0;
+		int loop = 0;
+		uint8_t code_buf[5] = {0xFF};
+		do {
+			read_len = serial->read(code_buf, 1, ASYNC);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+				loop++;
+			}
+		} while (code_buf[0] != PXT_PACKET_START && loop < 50000);
+		
+		if (read_len == 0 || read_len == MICROBIT_NO_DATA) return false;
+			
+		//read_len = serial->read(&code_buf[1], 4);
+		int readidx = 1;
+		do {
+			read_len = serial->read(&code_buf[readidx], 1, ASYNC);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA)
+				loop++;
+			else
+				readidx++;
+		} while (readidx < 5 && loop < 50000);
 
+		if (code_buf[0] == PXT_PACKET_START &&
+			code_buf[4] == PXT_PACKET_END &&
+			code_buf[2] == PXT_RET_CAM_SUCCESS)
+			return true;
+		return false;
+	}
+	
 	bool opencam(bool reset) 
 	{
 		//if (reset)
@@ -133,15 +268,14 @@ namespace pixetto {
 		
 		return false;
 	}
-	
     //% 
     bool begin(PixSerialPin rx, PixSerialPin tx){
 		bOnStarting = true;
-		bool ret = false;
-
-		PinName txn, rxn;
+		
 		//uBit.init();
 		
+		bool ret = false;
+		PinName txn, rxn;
 		//uBit.sleep(3000);
 		
 		if (getPinName(tx, txn) && getPinName(rx, rxn))
@@ -164,4 +298,497 @@ namespace pixetto {
 			
 		return ret;
     }
+    
+    
+    int test_opencam(bool reset) 
+	{
+		//if (reset)
+		//	uBit.sleep(8000);
+			
+		int ret = 0;
+		int try_streamon = 0;
+		do {
+			ssflush();
+			
+			uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON_CB, 0, PXT_PACKET_END};
+			serial->send(cmd_buf, 5, ASYNC);
+			
+			int read_len = 0;
+			uint8_t code_buf[5] = {0xFF};
+			
+			/*
+			do {
+				read_len = serial->read(code_buf, 1, ASYNC);
+				
+				if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+					loop++;
+				}
+			} while (code_buf[0] != PXT_PACKET_START && loop < 50000); 
+			*/
+			
+			read_len = ssread(code_buf, 1, 50000);
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA) return 1;
+			
+			/*
+			//read_len = serial->read(&code_buf[1], 4);
+			int readidx = 1;
+			do {
+				read_len = serial->read(&code_buf[readidx], 1, ASYNC);
+				
+				if (read_len == 0 || read_len == MICROBIT_NO_DATA)
+					loop++;
+				else
+					readidx++;
+			} while (readidx < 5 && loop < 50000);
+			*/
+			read_len = ssread(&code_buf[1], 4, 50000);
+
+			if (read_len == 4 &&
+			    code_buf[0] == PXT_PACKET_START &&
+				code_buf[4] == PXT_PACKET_END &&
+				code_buf[2] == PXT_RET_CAM_SUCCESS)
+				return 2;
+				
+			if (code_buf[0] == PXT_PACKET_START &&
+				code_buf[4] == PXT_PACKET_END &&
+				code_buf[2] == PXT_RET_CAM_ERROR)
+				ret = 3;
+
+			try_streamon++;
+			//uBit.sleep(500);
+		} while (try_streamon < 4);
+
+		if (ret > 0) return ret;
+		
+		return 4;
+	}
+    //% 
+    int test_begin(PixSerialPin rx, PixSerialPin tx){
+		bOnStarting = true;
+		
+		int ret = false;
+		PinName txn, rxn;
+		//uBit.sleep(3000);
+		
+		if (getPinName(tx, txn) && getPinName(rx, rxn))
+		{
+			serial = new MicroBitSerial(txn, rxn, 64, 20);
+
+			#if MICROBIT_CODAL
+			serial->setBaudrate(38400);
+			#else
+			serial->baud(38400);
+			#endif
+			
+			//serial->setRxBufferSize(64);
+			//serial->setTxBufferSize(32);
+			//uBit.sleep(100);
+			
+			ret = test_opencam(false);
+		}
+		if (ret)
+			bOnStarting = false;
+			
+		return ret;
+    }
+
+	//%
+    bool isDetected(){
+		if (bOnStarting) 
+			return false;
+		
+		int a = 0;
+		for (a=0; a<DATA_SIZE; a++)
+			data_buf[a] = 0xFF;
+	
+		ssflush();
+		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_QUERY, 0, PXT_PACKET_END};
+		serial->send(cmd_buf, 5, ASYNC);
+
+		int read_len = 0;
+		int loop = 0;
+		/*
+		do {
+			read_len = serial->read(data_buf, 1, ASYNC);
+			loop++;
+			//uBit.sleep(100);
+		} while (data_buf[0] != PXT_PACKET_START && loop < 300000);
+		*/
+		do {
+			read_len = ssread(data_buf, 1, 50000);
+			loop++;
+		} while (data_buf[0] != PXT_PACKET_START && loop < 300000);
+		
+		if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+			if (!checkcam()) {
+				uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_RESET, 0, PXT_PACKET_END};
+				serial->send(cmd_buf, 5, ASYNC);
+				opencam(true);
+			}
+			return false;
+		}
+
+		read_len = ssread(&data_buf[1], 2, 50000);
+		data_len = data_buf[1];
+		if (data_len > 3)
+			read_len = ssread(&data_buf[3], data_len - 3, 50000);
+		else
+			return false;
+		
+		if (read_len != (data_len-3)) return false;
+		if (data_buf[data_len-1] != PXT_PACKET_END) return false;
+		if (!verifyChecksum(data_buf, data_len)) return false;
+		if (data_buf[2] == 0) return false; // num == 0
+		
+		
+		if (data_buf[2] == DIGITS_OPERATION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
+			m_w = data_buf[5];
+			m_h = data_buf[6];
+			
+			m_eqAnswer = 0;
+			for (a=8; a<=14; a++) 
+				m_eqAnswer = m_eqAnswer * 10 + data_buf[a];
+
+			m_eqAnswer /= 100;
+			if (data_buf[7] == 0) m_eqAnswer = 0 - m_eqAnswer;
+			
+			memset(m_eqExpr, 0, sizeof(m_eqExpr));
+			m_eqLen = data_len - 17;
+			for (a=0; a<m_eqLen; a++)
+				m_eqExpr[a] = (char)data_buf[a+15];
+		}
+		else if (data_buf[2] == LANES_DETECTION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
+		}
+		else if (data_buf[2] == SIMPLE_CLASSIFIER) {
+			m_type = data_buf[3] * 256 + data_buf[4];
+			m_x = data_buf[5];
+			m_y = data_buf[6];
+			m_w = data_buf[7];
+			m_h = data_buf[8];
+		}
+		else {
+			m_type = data_buf[3];
+			m_x = data_buf[4];
+			m_y = data_buf[5];
+			m_w = data_buf[6];
+			m_h = data_buf[7];
+			
+			if (data_buf[2] == APRILTAG) {
+				int value = 0;
+				value = (short)(data_buf[8] * 256 + data_buf[9]);
+				m_posx = (float)value / 100.0;
+				value = (short)(data_buf[10] * 256 + data_buf[11]);
+				m_posy = (float)value / 100.0;
+				value = (short)(data_buf[12] * 256 + data_buf[13]);
+				m_posz = (float)value / 100.0;
+				
+				m_rotx = (short)(data_buf[14] * 256 + data_buf[15]);
+				m_roty = (short)(data_buf[16] * 256 + data_buf[17]);
+				m_rotz = (short)(data_buf[18] * 256 + data_buf[19]);
+				
+				m_centerx = (short)(data_buf[20] * 256 + data_buf[21]);
+				m_centery = (short)(data_buf[22] * 256 + data_buf[23]);
+			}
+		}
+		return true;
+	}
+
+	//%
+	int isTested(){
+		if (bOnStarting) 
+			return -1;
+		
+		int a = 0;
+		for (a=0; a<DATA_SIZE; a++)
+			data_buf[a] = 0xFF;
+	
+		ssflush();
+		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_QUERY, 0, PXT_PACKET_END};
+		serial->send(cmd_buf, 5, ASYNC);
+		
+		/*
+		do {
+			read_len = serial->read(data_buf, 1, ASYNC);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+				loop++;
+			}
+		} while (data_buf[0] != PXT_PACKET_START && loop < 300000);
+		*/
+		int read_len = 0;
+		int loop = 0;
+		do {
+			read_len = ssread(data_buf, 1, 50000);
+			loop++;
+		} while (data_buf[0] != PXT_PACKET_START && loop < 300000);
+		
+		if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+			if (!checkcam()) {
+				uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_RESET, 0, PXT_PACKET_END};
+				serial->send(cmd_buf, 5, ASYNC);
+				opencam(true);
+			}
+			return -2;
+		}
+		/*
+		read_len = serial->read(&data_buf[1], 2);//, ASYNC); // <len, func_id>
+		data_len = data_buf[1];
+		if (data_len > 3)
+			read_len = serial->read(&data_buf[3], data_len - 3);//, ASYNC);
+		else
+			return 1;
+		*/
+		/*
+		int readidx = 1;
+		do {
+			read_len = serial->read(&data_buf[readidx], 1, ASYNC);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA)
+				loop++;
+			else
+				readidx++;
+		} while (readidx < 3 && loop < 50000);		
+		*/
+		read_len = ssread(&data_buf[1], 2, 50000);
+		
+		data_len = data_buf[1];
+		if (data_len > 3){
+			//read_len = serial->read(&data_buf[3], data_len - 3);//, ASYNC);
+			/*
+			readidx = 3;
+			do {
+				read_len = serial->read(&data_buf[readidx], 1, ASYNC);
+				
+				if (read_len == 0 || read_len == MICROBIT_NO_DATA)
+					loop++;
+				else
+					readidx++;
+			} while (readidx < data_len && loop < 50000);		
+			*/
+			read_len = ssread(&data_buf[3], data_len - 3, 50000);
+		}
+		else
+			return 1;
+
+		if (read_len != (data_len-3)) return 2;
+		if (data_buf[data_len-1] != PXT_PACKET_END) return 3;
+		if (!verifyChecksum(data_buf, data_len)) return 4;
+		if (data_buf[2] == 0) return 5; // num == 0
+		
+		
+		if (data_buf[2] == DIGITS_OPERATION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
+			m_w = data_buf[5];
+			m_h = data_buf[6];
+			
+			m_eqAnswer = 0;
+			for (a=8; a<=14; a++) 
+				m_eqAnswer = m_eqAnswer * 10 + data_buf[a];
+
+			m_eqAnswer /= 100;
+			if (data_buf[7] == 0) m_eqAnswer = 0 - m_eqAnswer;
+			
+			memset(m_eqExpr, 0, sizeof(m_eqExpr));
+			m_eqLen = data_len - 17;
+			for (a=0; a<m_eqLen; a++)
+				m_eqExpr[a] = (char)data_buf[a+15];
+		}
+		else if (data_buf[2] == LANES_DETECTION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
+		}
+		else if (data_buf[2] == SIMPLE_CLASSIFIER) {
+			m_type = data_buf[3] * 256 + data_buf[4];
+			m_x = data_buf[5];
+			m_y = data_buf[6];
+			m_w = data_buf[7];
+			m_h = data_buf[8];
+		}
+		else {
+			m_type = data_buf[3];
+			m_x = data_buf[4];
+			m_y = data_buf[5];
+			m_w = data_buf[6];
+			m_h = data_buf[7];
+			
+			if (data_buf[2] == APRILTAG) {
+				int value = 0;
+				value = (short)(data_buf[8] * 256 + data_buf[9]);
+				m_posx = (float)value / 100.0;
+				value = (short)(data_buf[10] * 256 + data_buf[11]);
+				m_posy = (float)value / 100.0;
+				value = (short)(data_buf[12] * 256 + data_buf[13]);
+				m_posz = (float)value / 100.0;
+				
+				m_rotx = (short)(data_buf[14] * 256 + data_buf[15]);
+				m_roty = (short)(data_buf[16] * 256 + data_buf[17]);
+				m_rotz = (short)(data_buf[18] * 256 + data_buf[19]);
+				
+				m_centerx = (short)(data_buf[20] * 256 + data_buf[21]);
+				m_centery = (short)(data_buf[22] * 256 + data_buf[23]);
+			}
+		}
+		return 6;
+	}
+
+
+	//%
+	bool getFuncID(int func_id){
+		return (data_buf[2] == func_id);
+	}
+	
+    //% 
+    bool get_colordetect_color(int color) {
+		if (data_buf[2] == COLOR_DETECTION && data_buf[3] == color)
+			return true;
+        return false;
+    }
+    
+    //% 
+    bool get_shapedetect_shape(int shape) {
+		if (data_buf[2] == SHAPE_DETECTION && data_buf[3] == shape)
+			return true;
+        return false;
+    }
+
+    //% 
+    bool get_spheredetect_color(int color) {
+		if (data_buf[2] == SPHERE_DETECTION && data_buf[3] == color)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_nn_id(int id) {
+		if (data_buf[2] == NEURAL_NETWORK && data_buf[3] == id)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_traffic_sign(int sign) {
+		if (data_buf[2] == TRAFFIC_SIGN_DETECTION && data_buf[3] == sign)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_hand_digit(int digit) {
+		if (data_buf[2] == HANDWRITTEN_DIGITS_DETECTION && data_buf[3] == digit)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_hand_letter(int letter) {
+		if (data_buf[2] == HANDWRITTEN_LETTERS_DETECTION && data_buf[3] == letter)
+			return true;
+        return false;
+    }	
+
+    //%
+    bool get_remote_computing(int id) {
+		if (data_buf[2] == REMOTE_COMPUTING && data_buf[3] == id)
+			return true;
+        return false;
+    }
+	
+    //%
+    bool get_simple_classifier(int id) {
+		if (data_buf[2] == SIMPLE_CLASSIFIER && data_buf[3] == id)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_voice_command(int vcmd) {
+		if (data_buf[2] == VOICE_COMMANDS && data_buf[3] == vcmd)
+			return true;
+        return false;
+    }
+
+	//%
+	int getTypeID() {
+		return m_type;
+	}
+	//%
+	int getPosX() {
+		return m_x;
+	}
+	//%
+	int getPosY() {
+		return m_y;
+	}
+	//%
+	int getWidth() {
+		return m_w;
+	}
+	//%
+	int getHeight() {
+		return m_h;
+	}
+
+	//%
+	float getEquationAnswer() {
+		return m_eqAnswer;
+	}
+
+	/*//%
+	String getEquationExpr() {
+		ManagedString s = m_eqExpr;
+		return PSTR(s);
+	}*/
+	
+	//%
+	float getApriltagField(int field) {
+		switch(field) {
+			case APRILTAG_POS_X:
+				return m_posx;
+			case APRILTAG_POS_Y:
+				return m_posy;
+			case APRILTAG_POS_Z:
+				return m_posz;
+			case APRILTAG_ROT_X:
+				return m_rotx;
+			case APRILTAG_ROT_Y:
+				return m_roty;
+			case APRILTAG_ROT_Z:
+				return m_rotz;
+			case APRILTAG_CENTER_X:
+				return m_centerx;
+			case APRILTAG_CENTER_Y:
+				return m_centery;
+			default:
+				return 0;
+		}
+	}
+		
+	//%
+	int getLanesField(int field) {
+		switch(field) {
+			case LANES_LX1:
+				return data_buf[5];
+			case LANES_LY1:
+				return data_buf[6];
+			case LANES_LX2:
+				return data_buf[7];
+			case LANES_LY2:
+				return data_buf[8];
+			case LANES_RX1:
+				return data_buf[9];
+			case LANES_RY1:
+				return data_buf[10];
+			case LANES_RX2:
+				return data_buf[11];
+			case LANES_RY2:
+				return data_buf[12];
+			default:
+				return 0;
+		}
+	}
 }
